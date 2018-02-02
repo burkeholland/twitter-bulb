ddaac329e95a3d4c133d15909099e1101cb46a42
Burke Holland <burkeholland@Burkes-MacBook-Pro.local>
2018-02-02 15:23:34 -0600

    add color parameters

--- control-bulb/index.js
+++ control-bulb/index.js
@@ -4,33 +4,41 @@ require('dotenv').config();
 module.exports = function(context, req) {
   context.log('JavaScript HTTP trigger function processed a request.');
 
-  let client = new lifx({
-    bearerToken: process.env.LIFX_TOKEN
-  });
-
-  let options = {
-    color: 'blue',
-    from_color: 'white',
-    period: 2,
-    cycles: 5,
-    persist: false,
-    power_on: true
-  };
-
-  client
-    .breathe('all', options)
-    .then(result => {
-      context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: result
-      };
-
-      context.done();
-    })
-    .catch(err => {
-      context.res = {
-        status: 500,
-        body: result
-      };
+  if (req.query.color) {
+    let client = new lifx({
+      bearerToken: process.env.LIFX_TOKEN
     });
+
+    let options = {
+      color: req.query.color,
+      duration: 2,
+      power: true
+    };
+
+    client
+      .setState('all', options)
+      .then(result => {
+        context.res = {
+          // status: 200, /* Defaults to 200 */
+          body: result
+        };
+
+        context.done();
+      })
+      .catch(err => {
+        context.res = {
+          status: 500,
+          body: err
+        };
+
+        context.done();
+      });
+  } else {
+    context.res = {
+      body: 'Please pass a valid color',
+      status: 500
+    };
+
+    context.done();
+  }
 };
